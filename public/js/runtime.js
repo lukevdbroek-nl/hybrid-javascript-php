6f13a9eb9695b2c590b601dffdf80964b677d4ba
Luke van den Broek <contact@lukevdbroek.nl>
2025-10-26 02:08:01 +0200

    implemented client handle for event binds

--- public/js/runtime.js
+++ public/js/runtime.js
@@ -13,6 +13,18 @@ window.HJP = {
   }
 };
 
+// When DOM is mounted, a click appropriate event listener should be added to the action call
+window.addEventListener("DOMContentLoaded", () => {
+  events.forEach((event) => {
+    document.body.addEventListener(event, (e) => {
+      const actionName = e.target.dataset[`action${capitalize(event)}`];
+      if (actionName) {
+        HJP.call(actionName, e);
+      }
+    });
+  });
+});
+
 async function updateState(newState) {    
   const response = await fetch("../server/update.php", {
     method: "POST",
@@ -31,4 +43,8 @@ async function updateState(newState) {
     const text = await response.text();
     console.error("Invalid JSON from server:", text);
   }
+}
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
